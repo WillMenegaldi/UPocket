b3b4220a6f99fbeb878a0984b27cc9778962fedc
andrewcs0901 <andrewcs0901@gmail.com>
2019-06-09 21:31:57 -0300

    adicionada a visualização do valor gasto da categoria

--- js/orcamento.js
+++ js/orcamento.js
@@ -134,13 +134,13 @@ function listarOrcamentos() {
             `<div> 
                 <section id="lista-orcamento"> 
                     <div id="orcamentos" style="display: flex; flex-direction: row; margin-bottom: 3%;"> 
-                        <div id="categoria-orcamento" style="max-width:125px; width:125px">
+                        <div id="categoria-orcamento" style="max-width:125px; width:125px; margin-left: 3%">
                             ${catToString[orcamentos[i].idCategoria]} 
                         </div> 
                         <div id="valor-orcamento">
                             R$ ${orcamentos[i].valor.toFixed(2)} 
                         </div>
-                        <div id="valor-gasto">R$ 0,00</div> 
+                        <div id="valor-gasto${id}" style="max-width:125px; width:125px">R$ 0,00</div> 
                         <div id="box-progresso" style="margin-left: -10%;">
                             <div id="barra-progresso${id}">
                                 <script>progressBar()
@@ -280,7 +280,9 @@ function progressBar(){
 
     let despesas = db.filter(x => x.categoria != null);
     let progresso = 0;
+    let somaDespesa = 0;
     let id = 0;
+    console.log(somaDespesa);
 
     let orcamentoMensal = orcamentos.filter(x => x.mes == mes);
 
@@ -290,8 +292,12 @@ function progressBar(){
         categoriaDespesa = categoriaDespesa.filter(x => x.data.split("-")[1]  == mes);
         for(j=0; j<categoriaDespesa.length; j++)
         {
+            console.log(categoriaDespesa);
             orcamento = orcamentoMensal[i].valor;
             progresso += (categoriaDespesa[j].valor*100)/(orcamento);
+            somaDespesa = categoriaDespesa[j].valor;
+            let valor = ('valor-gasto'+ id).toString();
+            document.getElementById(valor).innerHTML = 'R$ '+somaDespesa;
         }
         let barra = ('barra-progresso'+ id).toString();
 
@@ -301,8 +307,8 @@ function progressBar(){
             }
             //document.getElementById(barra).style["background-image"] = 'linear-gradient(to-right, #6cf596,#efe946, #ff000094)';
         }
-
         document.getElementById(barra).style.width = progresso+'%';
+
         
         progresso = 0;
         id++;
